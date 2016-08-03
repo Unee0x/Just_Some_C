2f591e7c1eeb21b733bf5ef2a4251017daea2490
Unee0x <Unee0x@gmail.com>
2016-08-02 23:44:07 -0400

    rm_extra_spaces.c added

---
+++ rm_extra_spaces.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *rm_extra_spaces(char *s){
+	static char S[] = {'\0'};
+		for(int i = 0,j = 0; i < strlen(s);i++){
+		
+			while(*(s+i) == ' ' && *(s+i+1) == ' '){
+					i++;
+				}
+
+			S[j] = *(s+i);
+			j++;		
+	}
+	return S;
+}
+
+int main(int argc, char const *argv[])
+{
+	char *arg = "Today                              is                       Monday!";
+	char *s = rm_extra_spaces(arg);
+
+	printf("%s\n", s);
+
+	return 0;
+}
